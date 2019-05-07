729701a4877ffa66acbadeecec7e3d91ee3a5347
rpisipat <47987441+rpisipat@users.noreply.github.com>
2019-05-06 21:12:09 -0400

    Delete execute_script.py

--- execute_script.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-command1 = "final_submission_script.py" + sys.argv[1] + sys.argv[2] + sys.argv[3]
-command2 = "final_submission_script2.py" + sys.argv[1] + sys.argv[2] + sys.argv[3]
-
-
-execfile(command1)
-execfile(command2)
